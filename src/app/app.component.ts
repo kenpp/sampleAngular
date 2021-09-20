32ce50b1f47b39c76a447beb3f637dfaee6a04cd
TominagaKento <m153306s@st.u-gakugei.ac.jp>
2021-09-20 22:27:44 +0900

    背景色を付与したのと、いい感じにfor文で回せるようになった。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface Route {
+  path: string,
+  title: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'sampleAngular';
+  routes: Route[] = [
+    { path: "./simple-form", title: "Simple Form" },
+    { path: "./better-form", title: "Better Form" },
+    { path: "./controls", title: "Controls" },
+  ];
 }
