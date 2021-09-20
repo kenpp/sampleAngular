055f0375999e810daa34b039dd303ead051641de
TominagaKento <m153306s@st.u-gakugei.ac.jp>
2021-09-20 15:16:46 +0900

    プロパティの追加。

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./simple-form.component.css']
 })
 export class SimpleFormComponent implements OnInit {
-
+  result: string = "足し算しましょう。"
   constructor() { }
 
   ngOnInit(): void {
