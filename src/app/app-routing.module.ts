b045a4a3572526288ca0803511f1c988973e9835
TominagaKento <m153306s@st.u-gakugei.ac.jp>
2021-09-20 22:04:32 +0900

    ルーティングができるようになった！

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SimpleFormComponent } from './simple-form/simple-form.component';
+import { BetterFormComponent } from './better-form/better-form.component';
+import { ControlsComponent } from './controls/controls.component';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "simple-form", component: SimpleFormComponent },
+  { path: "better-form", component: BetterFormComponent },
+  { path: "controls", component: ControlsComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
