0a6f56740d5afc0be290f3b3803815ecc04cbd77
TominagaKento <m153306s@st.u-gakugei.ac.jp>
2021-09-20 20:45:26 +0900

    チェックボックスの追加

--- src/app/controls/controls.component.ts
+++ src/app/controls/controls.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-controls',
@@ -10,17 +10,31 @@ import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 export class ControlsComponent implements OnInit {
   coffeeForm!: FormGroup;
   hotColdStatuses: string[] = ["Hot", "Cold"];
+  addsSel: string[] = ["Milk", "Sugar"];
 
   // フォームビルダーを使うと、リアルタイムに値を設定することができる！！
   constructor(private fb: FormBuilder) {
     this.coffeeForm = this.fb.group({
       name: "ブレンド",
       taste: "バランスのよい口当たり",
-      hotCold: this.hotColdStatuses[0]
+      hotCold: this.hotColdStatuses[0],
+      adds: this.fb.array([])
     });
   }
 
   ngOnInit(): void {
+  }
+
+  // 本当は、checkedを渡したかったけど、できなかったため、このようにした。
+  onCheckChanged(item: string, event: any) {
+    const isChecked = event.target.checked as boolean;
+    let formArray = this.coffeeForm.controls.adds as FormArray;
+    if (isChecked) {
+      formArray.push(new FormControl(item));
+    } else {
+      let index = formArray.controls.findIndex(elm => elm.value == item);
+      formArray.removeAt(index);
+    }
   }
 
 }
