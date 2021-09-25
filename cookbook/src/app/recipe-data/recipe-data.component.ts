46f25a68ccb6276ac64792f2d627c11fe449c942
TominagaKento <m153306s@st.u-gakugei.ac.jp>
2021-09-25 18:23:40 +0900

    レシピを選択して、画像を表示させることができるようになった！

--- cookbook/src/app/recipe-data/recipe-data.component.ts
+++ cookbook/src/app/recipe-data/recipe-data.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { Recipe } from '../recipe/recipe';
+import { RECIPE_DATA } from '../recipe/recipeData';
 @Component({
   selector: 'app-recipe-data',
   templateUrl: './recipe-data.component.html',
@@ -8,11 +10,16 @@ import { Location } from '@angular/common';
 })
 export class RecipeDataComponent implements OnInit {
   recipeId: string | null;
+  recipe: Recipe;
 
   constructor(private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit(): void {
     this.recipeId = this.route.snapshot.paramMap.get("id");
+    const recipe = RECIPE_DATA.find(recipe => recipe.id.toString() == this.recipeId);
+    if (recipe) {
+      this.recipe = recipe;
+    }
   }
 
   public backToList() {
