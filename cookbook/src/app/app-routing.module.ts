7eeb6cd2036d8b8654d83d44bab5ce066b44d388
TominagaKento <m153306s@st.u-gakugei.ac.jp>
2021-09-25 17:54:19 +0900

    URLパラメータを渡すことができるようになった。

--- cookbook/src/app/app-routing.module.ts
+++ cookbook/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDataComponent } from './recipe-data/recipe-data.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "/recipe-list", pathMatch: "full" },
-  { path: "recipe-list", component: RecipeListComponent }
+  { path: "recipe-list", component: RecipeListComponent },
+  { path: "recipe-data/:id", component: RecipeDataComponent }
 ];
 
 @NgModule({
--- cookbook/src/app/recipe-data/recipe-data.component.ts
+++ cookbook/src/app/recipe-data/recipe-data.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-recipe-data',
   templateUrl: './recipe-data.component.html',
   styleUrls: ['./recipe-data.component.css']
 })
 export class RecipeDataComponent implements OnInit {
+  recipeId: string | null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.recipeId = this.route.snapshot.paramMap.get("id");
   }
 
 }
