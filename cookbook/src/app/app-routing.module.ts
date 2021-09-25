5ef1e1abef88ee3a7ee3787e5c0ee7738713f84c
TominagaKento <m153306s@st.u-gakugei.ac.jp>
2021-09-25 16:56:58 +0900

    ルートの追加

--- cookbook/src/app/app-routing.module.ts
+++ cookbook/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/recipe-list", pathMatch: "full" },
+  { path: "recipe-list", component: RecipeListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
