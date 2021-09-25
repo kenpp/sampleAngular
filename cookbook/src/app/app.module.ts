edb63cbfcbf22691f46c16da4c443b968f12c298
TominagaKento <m153306s@st.u-gakugei.ac.jp>
2021-09-25 16:37:38 +0900

    ディレクトリ移動&cookディレクトリの追加

---
+++ cookbook/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDataComponent } from './recipe-data/recipe-data.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RecipeListComponent,
+    RecipeDataComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
